28c529c04bafa63e841bc84a2f59f87a0f1b478b
vans140404 <moumbagnaevans@gmail.com>
2025-12-18 23:58:23 +0100

    Refactor comments in PremierOuNon function for clarity and conciseness

--- bibliotheque.c
+++ bibliotheque.c
@@ -1,4 +1,7 @@
-#include "bibliotheque.h" 
+#include "bibliotheque.h"
+#include<math.h> 
+#include <stdio.h>
+#include <stdlib.h> 
 // Définition d'une fonction de calcul de factorielle. 
 unsigned int fact( unsigned int value ) { 
 unsigned int result = 1; 
@@ -25,6 +28,58 @@ void permutation(int *a, int *b)
   int c = 0;
       c = *a;
       *a = *b;
-      *b = c;
-      
+      *b = c;    
 }
+// définition d'une fonction du sécond dégrer 
+void equationSecondDegre(float a, float b, float c, float *x1, float *x2, float *re, float *im, int *nombreSolutions)
+{
+    float d;
+
+    if (a == 0)
+    {
+        *nombreSolutions = -1;
+        return;
+    }
+
+    d = b*b - 4*a*c;
+
+    if (d > 0)
+    {
+        *x1 = (-b + sqrt(d)) / (2*a);
+        *x2 = (-b - sqrt(d)) / (2*a);
+        *nombreSolutions = 2;
+    }
+    else if (d == 0)
+    {
+        *x1 = -b / (2*a);
+        *nombreSolutions = 1;
+    }
+    else
+    {
+        *re = -b / (2*a);
+        *im = sqrt(-d) / (2*a);
+        *nombreSolutions = 0;
+    }
+}
+// définition d'une foncion qui lit un entier positif et vérifie si celui-ci est premier
+int PremierOuNon(int n) {
+    int i;
+    if (n <= 1) return 0; 
+    if (n == 2) return 1;  
+    if (n % 2 == 0) return 0;  
+    for (i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return 0;  
+        }
+    }
+    
+    return 1;  
+}
+// définition d'une fonction qui calcule et affiche une distance
+int CalculeEtAfficheDistance(float x1, float y1, float x2, float y2) {
+    float distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("La distance entre les points (%.2f, %.2f) et (%.2f, %.2f) est : %.2f\n",
+           x1, y1, x2, y2, distance);
+    
+    return 0;
+}    
--- bibliotheque.h
+++ bibliotheque.h
@@ -4,6 +4,12 @@
 unsigned int fact( unsigned int value ); 
 // Définition d'une fonction d'élévation à une puissance données. 
 int power( int value, unsigned int pow );
-// définition d'une fonction de permutation 
+// Définition d'une fonction de permutation 
 void permutation(int *a, int *b);
+// Définition d'une fonction du second dégrer
+void equationSecondDegre(float a, float b, float c, float *x1, float *x2, float *re, float *im, int *nombreSolutions);
+// Définition d'une foncion qui lit un entier positif et vérifie si celui-ci est premier 
+int PremierOuNon(int n);
+// Definition d'une fonction qui calcule et affiche une distance 
+int CalculeEtAfficheDistance(float x1, float y1, float x2, float y2);
 #endif
